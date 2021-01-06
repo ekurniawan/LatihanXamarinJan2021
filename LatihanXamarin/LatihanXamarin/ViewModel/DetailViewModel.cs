7bbf6fd1ee74f1aafaf392217fd10f6727f2287a
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-01-06 14:00:36 +0700

    menambahkan delete dan edit data

--- LatihanXamarin/LatihanXamarin/ViewModel/DetailViewModel.cs
+++ LatihanXamarin/LatihanXamarin/ViewModel/DetailViewModel.cs
@@ -1,7 +1,10 @@
-﻿using MvvmHelpers;
+﻿using LatihanXamarin.DAL;
+using LatihanXamarin.Models;
+using MvvmHelpers;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace LatihanXamarin.ViewModel
@@ -10,10 +13,72 @@ namespace LatihanXamarin.ViewModel
     {
         public Command EditCommand { get; }
         public Command DeleteCommand { get; }
+        private EmployeeDAL empDAL;
 
         public DetailViewModel()
         {
             Title = "Detail Employee";
+            empDAL = new EmployeeDAL();
+            EditCommand = new Command(async ()=> await EditCommandExecute());
+            DeleteCommand = new Command(async ()=> await DeleteCommandExecute());
+        }
+
+        async Task DeleteCommandExecute()
+        {
+            try
+            {
+                var answer = await Application.Current.MainPage.DisplayAlert("Konfirmasi",
+                    "Apakah anda yakin akan delete data?", "Yes", "No");
+
+                if (!answer)
+                    return;
+
+                var deleteData = new Employee
+                {
+                    EmpId = this.EmpId
+                };
+
+                var result = empDAL.Delete(deleteData);
+
+                if (result == 1)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Keterangan",
+                      $"Data Employee berhasil di delete", "OK");
+                    await Application.Current.MainPage.Navigation.PopAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error",
+                        $"Error {ex.Message}", "OK");
+            }
+        }
+
+        async Task EditCommandExecute()
+        {
+            try
+            {
+                var editData = new Employee
+                {
+                    EmpId = this.EmpId,
+                    EmpName = this.EmpName,
+                    Department = this.Department,
+                    Designation = this.Designation,
+                    Qualification = this.Qualification
+                };
+                var result = empDAL.Edit(editData);
+                if (result == 1)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Keterangan",
+                       $"Data Employee {editData.EmpName} berhasil di edit", "OK");
+                    await Application.Current.MainPage.Navigation.PopAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error",
+                        $"Error {ex.Message}", "OK");
+            }
         }
 
         private int empId;
