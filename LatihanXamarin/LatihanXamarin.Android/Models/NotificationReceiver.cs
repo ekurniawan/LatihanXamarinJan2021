6d2ee5f59e21b3a35770913c8f1542b036a95f01
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-01-07 10:16:27 +0700

    menambahkan notifreceived

--- LatihanXamarin/LatihanXamarin.Android/Models/NotificationReceiver.cs
+++ LatihanXamarin/LatihanXamarin.Android/Models/NotificationReceiver.cs
@@ -4,14 +4,30 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using LatihanXamarin.Droid.Models;
+using LatihanXamarin.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
+[assembly: Xamarin.Forms.Dependency(typeof(NotificationReceiver))]
 namespace LatihanXamarin.Droid.Models
 {
-    public class NotificationReceiver
+    public class NotificationReceiver : INotificationReceiver
     {
+        public event EventHandler<string> NotificationReceived;
+        public event EventHandler<string> ErrorReceived;
+
+        public void RaiseErrorReceived(string message)
+        {
+            ErrorReceived?.Invoke(this, message);
+        }
+
+        public void RaiseNotificationReceived(string message)
+        {
+            NotificationReceived?.Invoke(this, message);
+        }
     }
 }
