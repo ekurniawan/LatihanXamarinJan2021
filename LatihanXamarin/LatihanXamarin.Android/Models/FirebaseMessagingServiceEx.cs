04a8684e66b0968d2f1050749b41bd12f93b791b
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-01-07 10:49:29 +0700

    meanbamhkan firebaseservices ex

---
+++ LatihanXamarin/LatihanXamarin.Android/Models/FirebaseMessagingServiceEx.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Messaging;
+using LatihanXamarin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace LatihanXamarin.Droid.Models
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class FirebaseMessagingServiceEx : FirebaseMessagingService
+    {
+        private const string Template = "{\"notification\":{\"body\":\"$(body)\",\"title\":\"$(title)\"},\"data\":{\"title\":\"$(title)\",\"body\":\"$(body)\"}}";
+
+        public override void OnNewToken(string token)
+        {
+            base.OnNewToken(token);
+        }
+
+        public override void OnMessageReceived(RemoteMessage remoteMessage)
+        {
+            base.OnMessageReceived(remoteMessage);
+
+            var receiver = DependencyService.Get<INotificationReceiver>();
+            var message = remoteMessage.Data["body"];
+            receiver.RaiseNotificationReceived(message);
+        }
+    }
+}
