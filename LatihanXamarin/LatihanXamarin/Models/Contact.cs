0425160668971b1677f26de6bbb053dc26242387
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-01-07 10:11:11 +0700

    menambahkan dependency services

--- LatihanXamarin/LatihanXamarin/Models/Contact.cs
+++ LatihanXamarin/LatihanXamarin/Models/Contact.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace LatihanXamarin.Models
-{
+{//
     class Contact
     {
     }
---
+++ LatihanXamarin/LatihanXamarin/Models/INotificationReceiver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LatihanXamarin.Models
+{
+    public interface INotificationReceiver
+    {
+        event EventHandler<string> NotificationReceived;
+        event EventHandler<string> ErrorReceived;
+        void RaiseNotificationReceived(string message);
+        void RaiseErrorReceived(string message);
+    }
+}
